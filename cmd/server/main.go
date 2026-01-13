935e25cf986a8cf70b82470f5e9e7d64ae274954
pk70 <sajibg7@gmail.com>
2026-01-13 19:11:57 +0600

    feature:add test runing the server with specific port

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,25 @@ package main
 import (
 	"ecommerce/config"
 	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 func main() {
 	cnf := config.GetEnv()
-	fmt.Print(cnf)
+	portStr := cnf.HttpPort
+	port := strconv.Itoa(portStr)
+	fmt.Printf("Starting server on :%s\n", port)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /api/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello! I am invisible")
+	})
+	// Start server
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 
 }
